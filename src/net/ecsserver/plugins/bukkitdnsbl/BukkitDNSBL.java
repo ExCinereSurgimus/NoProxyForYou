0b3b0373504bbb0666288311d8236accd949c153
Dustin Essington <aetaric@gmail.com>
2013-07-08 02:23:40 -0400

    Fixed a small issue where player's might not get checked
    Incremented version to 1.0.1 for small bug fix.

--- src/net/ecsserver/plugins/bukkitdnsbl/BukkitDNSBL.java
+++ src/net/ecsserver/plugins/bukkitdnsbl/BukkitDNSBL.java
@@ -16,8 +16,10 @@
 
 package net.ecsserver.plugins.bukkitdnsbl;
 
+import java.util.ArrayList;
 import java.util.logging.Logger;
 
+import org.bukkit.Server;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -25,12 +27,17 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class BukkitDNSBL extends JavaPlugin implements Listener {
+	private static boolean DEBUG = false; // set to false for release builds!
+	
 	public final static Logger log = Logger.getLogger("DNSBL");
 	public static String logPrefix = "[DNSBL]";
 	public static String chatPrefix = "&3[DNSBL]&f";
+	public Server server = getServer();
+	public static ArrayList<String> list = new ArrayList<String>(); // no, No, NO, NOOOO!
 	
 	@Override
     public void onEnable() {
+		getServer().getPluginManager().registerEvents(this, this);
         log.info(logPrefix + " now checking players against know DNSBLs.");
     }
  
@@ -39,11 +46,20 @@ public class BukkitDNSBL extends JavaPlugin implements Listener {
         log.info(logPrefix + " Disabling!");
     }
     
+	public static void debugLog(String string) {
+		if (DEBUG == true) {
+			log.info(string);
+		}
+	}
+    
 	@EventHandler
 	void OnPlayerJoin(PlayerJoinEvent event) {
+		debugLog(logPrefix + " Got login for player " + event.getPlayer().getName());
 		Player player = event.getPlayer();
 		if (!player.hasPermission("dnsbl.ignore") || !player.isOp()) {
-			new DNSBLThread(player, this).runTaskAsynchronously(this);
+			debugLog(logPrefix + " Player is not op... Checking player " + player.getName());
+			list.add(player.getName());
+			new DNSBLThread(this).runTaskAsynchronously(this);
 		}
 	}
 }
--- src/net/ecsserver/plugins/bukkitdnsbl/DNSBLThread.java
+++ src/net/ecsserver/plugins/bukkitdnsbl/DNSBLThread.java
@@ -18,59 +18,86 @@ package net.ecsserver.plugins.bukkitdnsbl;
 
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.util.ArrayList;
 import java.util.logging.Logger;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import org.xbill.DNS.ARecord;
+import org.xbill.DNS.CNAMERecord;
 import org.xbill.DNS.Lookup;
 import org.xbill.DNS.Record;
 import org.xbill.DNS.Type;
 
 public class DNSBLThread extends BukkitRunnable {
-
-	private Player player;
+	
+	private static Player player;
+	private static Plugin plugin;
 	private Logger log;
-	private Plugin plugin;
 	private String chatPrefix;
 	private String logPrefix;
+	private ArrayList<String> list;
 
-	public DNSBLThread(Player player, Plugin plugin) {
-		player = this.player;
+	public DNSBLThread(Plugin plugin) {
+		list = BukkitDNSBL.list;
 		log = BukkitDNSBL.log;
 		chatPrefix = BukkitDNSBL.chatPrefix;
 		logPrefix = BukkitDNSBL.logPrefix;
-		plugin = this.plugin;
-		
+		plugin = DNSBLThread.plugin;
 	}
-
-	@Override
+	
 	public void run() {
+		checkIP(DNSBLThread.player);
+	}
+	
+	public void checkIP(Player player) {
+		// FIXME: I MUST FIND A BETTER WAY TO DO THIS!
+		player = Bukkit.getPlayer(list.get(0));
+		list.clear();
+		
+		BukkitDNSBL.debugLog(logPrefix + " Player " + player.getName().toString());
+		
 		InetSocketAddress sockAddr = player.getAddress();
 		InetAddress ip = sockAddr.getAddress();
-		String[] breakTehIP = ip.getHostAddress().replace("/","").split(".");
-		String reverseIP = breakTehIP[3] + "." + breakTehIP[2]+ "." + breakTehIP[1]+ "." + breakTehIP[0];
+		
+		BukkitDNSBL.debugLog(logPrefix + " IP is " + ip.getHostAddress());
+		String[] splitIP = ip.getHostAddress().split("\\.");
+		BukkitDNSBL.debugLog(logPrefix + " String Array is " + splitIP.length); 
+		for (String i : splitIP){
+			BukkitDNSBL.debugLog(logPrefix + i);
+		}
+		String reverseIP = splitIP[3] + "." + splitIP[2]+ "." + splitIP[1]+ "." + splitIP[0];
 		
 		// Check DroneBL
 		try {
+			BukkitDNSBL.debugLog(logPrefix + " Checking DroneBL for ip " + ip.toString().replace("/", ""));
 			Lookup lookup = new Lookup(reverseIP + ".dnsbl.dronebl.org", Type.A);
 			Record[] records = lookup.run();
 			if (records != null) {
 				for (Record record : records) {
 					ARecord a = (ARecord) record;
 					if (a.getAddress().equals(InetAddress.getByName("127.0.0.8"))) { // socks4/5 proxy = 127.0.0.8
+						BukkitDNSBL.debugLog(logPrefix + " We've found a socks proxy in dronebl! Kicking player " + player.getName());
 						player.kickPlayer("You are listed in DroneBL. Are you a proxy? http://dronebl.org/lookup_branded?ip=" + ip.toString().replace("/",""));
+						return;
 					}
 					else if (a.getAddress().equals(InetAddress.getByName("127.0.0.9"))) { // http proxy = 127.0.0.9
+						BukkitDNSBL.debugLog(logPrefix + " We've found a http proxy in dronebl! Kicking player " + player.getName());
 						player.kickPlayer("You are listed in DroneBL. Are you a proxy? http://dronebl.org/lookup_branded?ip=" + ip.toString().replace("/",""));
+						return;
 					}
 					else if (a.getAddress().equals(InetAddress.getByName("127.0.0.10"))) { // proxy chain = 127.0.0.10
+						BukkitDNSBL.debugLog(logPrefix + " We've found a proxy chain in dronebl! Kicking player " + player.getName());
 						player.kickPlayer("You are listed in DroneBL. Are you a proxy? http://dronebl.org/lookup_branded?ip=" + ip.toString().replace("/",""));
+						return;
 					}
 					else if (a.getAddress().equals(InetAddress.getByName("127.0.0.14"))) { // open wingate proxy = 127.0.0.14
+						BukkitDNSBL.debugLog(logPrefix + " We've found a winegate proxy in dronebl! Kicking player " + player.getName());
 						player.kickPlayer("You are listed in DroneBL. Are you a proxy? http://dronebl.org/lookup_branded?ip=" + ip.toString().replace("/",""));
+						return;
 					}
 					else { // catch all others (ddos bots and the like and link them to dronebl
 						player.sendMessage(chatPrefix + " You are listed in DroneBL. To find out more goto: http://dronebl.org/lookup_branded?ip=" + ip.toString().replace("/",""));
@@ -85,15 +112,18 @@ public class DNSBLThread extends BukkitRunnable {
 		
 		// Check sectoor: Tor exit node BlackList
 		try {
-			Lookup lookup = new Lookup(reverseIP + ".tor.dnsbl.sectoor.de", Type.A);
+			BukkitDNSBL.debugLog(logPrefix + " Checking sectoor for ip " + ip.toString().replace("/", ""));
+			Lookup lookup = new Lookup(reverseIP + ".tor.dnsbl.sectoor.de", Type.CNAME);
 			Record[] records = lookup.run();
 			if (records != null) {
 				for (Record record : records) {
-					ARecord a = (ARecord) record;
-					if (a.getAddress().equals(InetAddress.getByName("127.0.0.1"))) {
+					CNAMERecord a = (CNAMERecord) record;
+					if (a.getName().equals("torserver.tor.dnsbl.sectoor.de")) {
+						BukkitDNSBL.debugLog(logPrefix + " We've found a tor exit node in sectoor.de! Kicking player " + player.getName());
 						player.kickPlayer("You are listed in sectoor. Are you a tor exit node?");
+						return;
 					}
-					if (a.getAddress().equals(InetAddress.getByName("127.0.0.2"))) {
+					if (a.getName().equals("tornetwork.tor.dnsbl.sectoor.de")) {
 						// This is a tor exitnode on the network. wut do here?
 					}
 				}
@@ -106,10 +136,13 @@ public class DNSBLThread extends BukkitRunnable {
 		
 		// Check spamhaus' XBL list (proxies)
 		try {
+			BukkitDNSBL.debugLog(logPrefix + " Checking Spamhaus for ip " + ip.toString().replace("/", ""));
 			Lookup lookup = new Lookup(reverseIP + ".xbl.spamhaus.org", Type.A);
 			Record[] records = lookup.run();
 			if (records != null) {
+				BukkitDNSBL.debugLog(logPrefix + " We've found a record in spamhaus! Kicking player " + player.getName());
 				player.kickPlayer("You are listed in spamhaus' XBL list. Are you a proxy? You might have a virus...");
+				return;
 			}
 		}
 		catch (Exception e) {
@@ -119,10 +152,13 @@ public class DNSBLThread extends BukkitRunnable {
 		
 		// Check ProxyBL 
 		try {
+			BukkitDNSBL.debugLog(logPrefix + " Checking ProxyBL for ip " + ip.toString().replace("/", ""));
 			Lookup lookup = new Lookup(reverseIP + ".dnsbl.proxybl.org", Type.A);
 			Record[] records = lookup.run();
 			if (records != null) {
+				BukkitDNSBL.debugLog(logPrefix + " We've found a proxy in ProxyBL! Kicking player " + player.getName());
 				player.kickPlayer("You are listed in ProxyBL. Are you a proxy?");
+				return;
 			}
 		}
 		catch (Exception e) {
@@ -132,10 +168,13 @@ public class DNSBLThread extends BukkitRunnable {
 		
 		// Check Sorbs' proxy lists 
 		try {
+			BukkitDNSBL.debugLog(logPrefix + " Checking Sorbs for ip " + ip.toString().replace("/", ""));
 			Lookup lookup = new Lookup(reverseIP + ".proxies.dnsbl.sorbs.net", Type.A);
 			Record[] records = lookup.run();
 			if (records != null) {
+				BukkitDNSBL.debugLog(logPrefix + " We've found a proxy in sorbs! Kicking player " + player.getName());
 				player.kickPlayer("You are listed in Sorbs' Proxy Blacklist. Are you a proxy?");
+				return;
 			}
 		}
 		catch (Exception e) {
